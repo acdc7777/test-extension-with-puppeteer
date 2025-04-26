8e1c73f07735963f05d59dd5e4530602918996bc
acdc7777 <155995732+acdc7777@users.noreply.github.com>
2025-04-26 20:47:09 +0100

    Add files via upload

---
+++ pptr.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+
+const paths =__dirname+"./history/historyOverride";
+
+
+const opts = {
+  headless: false,
+  devtools: true,
+  args: [
+    `--disable-extensions-except=${paths}`,
+    `--load-extension=${paths}`,
+    `--window-size=800,600`
+  ]
+};
+
+(async () => {
+  try {
+    console.log('==>Open Browser');
+    const browser = await puppeteer.launch(opts);
+
+    console.log('==>Navigate to Extension');
+    const [page] = await browser.pages();
+
+
+    await page.goto('chrome://extensions/');
+
+    console.log('==> Take Screenshot');
+    await page.screenshot({ path: 'extension.png' });
+
+
+    await page.evaluate(() => {
+      alert('===== Exit =====');
+    });
+    await page.close();
+    await browser.close();
+
+  } catch (err) {
+    console.error(err);
+  }
+})
+
